464a03411db2363e1ec3e6ec5eba9fbf0056d84a
CDTester <chris_a_day@hotmail.com>
2026-03-26 16:42:56 +0000

    python math

---
+++ src/maths.py
@@ -0,0 +1,21 @@
+import math
+print("\nMATHS:")
+
+a = [5, 10, 25]
+print("a =", a)
+
+print("\nMin, Max, Abs")
+print("min(a) =", min(a))
+print("max(a) =", max(a))
+print("abs(-156.44) = ", abs(-156.44))
+
+
+print("\nPower - The pow(x, y) function returns the value of x to the power of y.")
+print("pow(4,3) =", pow(4,3))
+
+print("\nSquare root")
+print("math.sqrt(64) = ", math.sqrt(64))
+
+print("\nCeiling, Floor")
+print("math.ceil(1.4) = ", math.ceil(1.4))
+print("math.floor(1.4) = ", math.floor(1.4))

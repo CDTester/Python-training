c721cfd051831133f02e68c3435b7185d00dd2a3
CDTester <chris_a_day@hotmail.com>
2026-05-05 12:00:37 +0100

    added pathlib to files

--- src/files.py
+++ src/files.py
@@ -4,8 +4,11 @@ import os
 # r=Read (default), a=Append, w=Write, x=Create
 # file handling:
 # t=Text (default), b=binary 
+from pathlib import Path
 
-file1 = "../test_data/data1.txt"
+base_dir = Path(__file__).parent.parent
+print(f"base_dir: {base_dir}")
+file1 = base_dir / "test_data" / "data1.txt"
 
 read_text1 = open(file1) # r=read, t=Text by default
 print("\n.read()")
@@ -46,14 +49,14 @@ with open(file1) as f:
   print(f.read())
 
 print("\ncreate new file:")
-with open("../test_data/file2.txt", "x") as f:
+with open(base_dir / "test_data" / "file2.txt", "x") as f:
   f.write("line1\nline2\nline3")
 with open(file1) as f:
   print(f.read())
 
 print("\ndelete new file using os.remove:")
-if os.path.exists("../test_data/file2.txt"):
-  print("Removing file: ../test_data/file2.txt")
-  os.remove("../test_data/file2.txt")
+if os.path.exists(base_dir / "test_data" / "file2.txt"):
+  print("Removing file: {}".format(base_dir / "test_data" / "file2.txt"))
+  os.remove(base_dir / "test_data" / "file2.txt")
 else:
-  print("file ../test_data/file2.txt does not exist")
+  print("file {} does not exist".format(base_dir / "test_data" / "file2.txt"))
